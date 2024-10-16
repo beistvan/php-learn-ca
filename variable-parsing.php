2cce9f3745a88818e3864de7f8133aa1636565a8
beistvan <beistvan@gmail.com>
2024-10-17 01:48:08 +0200

    Create variable-parsing.php

---
+++ variable-parsing.php
@@ -0,0 +1,14 @@
+<?php
+// Fill in the blanks in the code below:
+  $noun = "thing";
+  $adjective = "nice";
+  $verb = "track";
+
+  echo "The world's most beloved $noun was very $adjective and loved to $verb every single day.";
+
+
+//Fix the code below and uncomment it:
+
+ echo "\nI have always been obsessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.";
+
+?>
