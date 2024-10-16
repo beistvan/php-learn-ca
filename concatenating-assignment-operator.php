ed20d8c87f8b1eb1ee0393baf6ed1b7f41a24445
beistvan <beistvan@gmail.com>
2024-10-17 01:49:26 +0200

    Create concatenating-assignment-operator.php

---
+++ concatenating-assignment-operator.php
@@ -0,0 +1,18 @@
+<?php
+  echo "I'm going on a picnic!";
+
+  $sentence = "\nI'm going on a picnic, and I'm taking apples";
+
+  echo $sentence;
+
+// Write your code below:
+  $sentence .= ", buy icecream";
+
+  echo $sentence;
+
+  $sentence .= ", conding in PHP";
+
+  echo $sentence;
+
+
+?>
