593eb9b832d1bae55a6d1d61cb25d19a68116c94
beistvan <beistvan@gmail.com>
2024-10-17 01:48:45 +0200

    Create variable-reassignment.php

---
+++ variable-reassignment.php
@@ -0,0 +1,17 @@
+<?php
+  $movie = "John Wick";
+// Add your code here:
+  $old_favorite = $movie;
+
+  echo "I'm a fickle person, my favorite movie used to be $movie.";
+  
+// Add a statement here:
+  $movie = "Terminator";
+  
+  echo "\nBut now my favorite is $movie.";
+  
+// Add a statement below:
+
+  echo "\nBut the $old_favorite is still nice.";
+  
+?>
