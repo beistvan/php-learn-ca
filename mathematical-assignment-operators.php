b87e3ea9ce7512334b8884cf92b05bd6f00fcd6e
beistvan <beistvan@gmail.com>
2024-10-20 01:00:17 +0200

    Mathematical Assignment Operators

---
+++ mathematical-assignment-operators.php
@@ -0,0 +1,19 @@
+<?php
+
+  $my_num = 12;
+
+  $answer = $my_num;
+
+  $answer += 2;
+  
+  $answer *= 2;
+  
+  $answer -= 2;
+  
+  $answer /= 2;
+  
+  $answer -= $my_num;
+  
+  echo $answer;
+  
+?>
