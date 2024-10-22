a36408c57f99939d613358364089fa63811ac7f3
beistvan <beistvan@gmail.com>
2024-10-23 01:05:47 +0200

    Multiple Parameters Of Functions

---
+++ multiple-parameters-of-functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function calculateArea($height, $width) 
+{ 
+  return $height * $width;
+}
+
+echo calculateArea(4, 5);
+
+function calculateVolume($height, $width, $depth)
+{ 
+  return $height * $width * $depth;
+}
+
+echo calculateVolume(3, 4, 5);
+?>
