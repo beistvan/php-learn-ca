74d746d442d40834b9624558814c0ad74ce665ec
beistvan <beistvan@gmail.com>
2024-10-23 01:02:07 +0200

    Return Values

---
+++ return-values.php
@@ -0,0 +1,19 @@
+<?php
+function first()
+{
+  return "You did it!\n";
+}
+
+function second()
+{
+  return "You're amazing!\n";
+}
+
+function third()
+{
+  return "You're a coding hero!\n";
+}
+
+
+echo first() . "\n" . second() . "\n" . third();
+?>
