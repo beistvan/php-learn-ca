b10678bdbd8468084c352b478f066933ec9fa492
beistvan <beistvan@gmail.com>
2024-10-29 13:39:27 +0100

    Built-in Functions - Working With Variables

---
+++ built-in-functions-working-with-variables.php
@@ -0,0 +1,17 @@
+<?php
+namespace Codecademy;
+
+$first = "Welcome to the magical world of built-in functions.";
+  
+$second = 82137012983; 
+
+
+echo gettype($first); 
+ 
+echo gettype($second); 
+
+var_dump($first); 
+ 
+var_dump($second); 
+
+?>
