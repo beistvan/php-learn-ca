506fe580c625629c9e7cc3e3889ff7df475fbdc2
beistvan <beistvan@gmail.com>
2024-11-03 00:40:05 +0100

    If Statements in PHP

---
+++ if-statements.php
@@ -0,0 +1,15 @@
+<?php
+namespace Codecademy;
+
+function markAnswer($is_correct) {
+  if($is_correct){
+    return "green";
+  } else {
+    return "red";
+  }
+}
+
+echo markAnswer(true);
+
+echo markAnswer(false);
+?>
