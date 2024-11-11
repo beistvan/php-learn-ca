0554ff72add775e4de32660846be0ffd46ef049e
beistvan <beistvan@gmail.com>
2024-11-11 11:06:49 +0100

    Ordered Arrays - More Array Methods: Pushing And Popping In PHP

---
+++ ordered-arrays-more-array-methods-pushing-and-popping.php
@@ -0,0 +1,11 @@
+<?php
+namespace Codecademy;
+
+$stack = ["wild success", "failure", "struggle"];
+
+$num_added = array_push($stack, "blocker", "impediment"); 
+array_pop($stack);
+array_pop($stack); 
+array_pop($stack); 
+$popped = array_pop($stack); 
+?>
