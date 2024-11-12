5b0ec90371ae87017163c7680a748f9030e0bf14
beistvan <beistvan@gmail.com>
2024-11-12 01:26:01 +0100

    Loops - For Loop In PHP

---
+++ loops-for.php
@@ -0,0 +1,21 @@
+<?php
+for ($i = 10; $i >= 0; $i--)
+{
+  if ($i > 2)
+  {
+    echo $i . "\n";
+  } 
+  elseif ($i == 2) 
+  {
+    echo "Ready!\n";
+  } 
+  elseif ($i == 1) 
+  {
+    echo "Set!\n";
+  }
+  else 
+  {
+    echo "Go!\n";
+  }
+}
+?>
