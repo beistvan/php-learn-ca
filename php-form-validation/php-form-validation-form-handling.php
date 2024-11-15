46843617ab6a9b34a6d88aac0db745d57b479395
beistvan <beistvan@gmail.com>
2024-11-15 20:02:28 +0100

    PHP Form Validation - Form Handling

---
+++ php-form-validation/php-form-validation-form-handling.php
@@ -0,0 +1,20 @@
+<html>
+	<body>
+		<form method="post" action="">
+      <div id="form-output">
+        <p id="name"> Hello, <?= $_POST["name"]?>! </p>
+        <p id="best"> I am glad you enjoy <?= $_POST["best"]?>. </p>
+      </div>
+			Your name:
+			<br>
+  		<input type="text" name="name">
+ 			<br><br>
+  		What is the best thing about learning to code:
+  		<br>
+  		<input type="text" name="best">
+  		<br><br> 
+  		<input type="submit" value="Submit Answers">
+		</form>
+		<a href="index.php">Reset</a>
+	</body>
+</html>
